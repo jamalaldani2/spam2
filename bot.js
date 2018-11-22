4cc6cf400951442874a1665ff05cb34e2f6cf956
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-11-22 19:32:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', async() => {
 var server = "515202282824925184"; // ايدي السررفر
 var channel = "515206756201136130";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd.xd :joy:**')
+    client.guilds.get(server).channels.get(channel).send('**xDream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
 })
 
